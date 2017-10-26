08549b9d53faf716db157262859e3069c03b4cd9
Hongliang <code@hongliang.nl>
2017-10-26 09:21:09 +0800

    bugfix

--- src/Voter/SpiderVoter.php
+++ src/Voter/SpiderVoter.php
@@ -6,12 +6,16 @@ class SpiderVoter extends BaseVoter implements VoterInterface
 {
     public function vote($spider = null)
     {
+        if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+            return false;
+        }
         $spiders = $this->getAssets();
         foreach ($spiders as $s) {
             if (false !== strpos($_SERVER['HTTP_USER_AGENT'], $s)) {
                 return true;
             }
         }
+
         return false;
     }
 
